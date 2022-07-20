ac369fe69995b3fea00eaaa618e01cc0304c5824
dsconnelly <dsconnelly@nyu.edu>
2022-07-20 17:10:35 -0400

    refactored and changed major version because n_validation is now val_size

--- mubofo/__init__.py
+++ mubofo/__init__.py
@@ -1,7 +1,7 @@
 """Boosted forest regression with native support for multioutput problems."""
 
-__version__ = '1.0.1'
+__version__ = '2.0.0'
 
 from ._models import BoostedForestRegressor
 
-__all__ = 'BoostedForestRegressor'
+__all__ = ['BoostedForestRegressor']
--- mubofo/_models.py
+++ mubofo/_models.py
@@ -1,26 +1,21 @@
 import numpy as np
 
 from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
+from ._utils import get_error, get_n_bootstrap
+
 class BoostedForestRegressor(BaseEstimator, RegressorMixin):
-    """
-    Boosted forest for (possibly multioutput) regression problems.
-
-    The BoostedForestRegressor class implements the scikit-learn estimator API
-    and can therefore be used like any other scikit-learn model.
-
-    """
-
     def __init__(
         self,
         n_estimators=100,
         learning_rate=0.2,
-        max_depth=5,
         max_samples=None,
+        max_depth=5,
         max_features=None,
-        n_validation=None,
+        val_size=None,
         threshold=0.01,
         patience=10,
         random_state=None,
@@ -36,31 +31,31 @@ class BoostedForestRegressor(BaseEstimator, RegressorMixin):
             fitting, n_estimators is the maximum number of trees.
         learning_rate : float
             The weight multiplying the output of each tree in the forest.
-        max_depth : int
-            The max depth of each tree in the forest.
         max_samples : None or int or float
             The size of each bootstrapped subsample of the dataset. If None,
             then each subsample will have n_samples rows. If an int, each
             subsample will have max_samples rows. If a float between 0 and 1,
             each subsample will have int(max_samples * n_sample) rows.
+        max_depth : int
+            The max depth of each tree in the forest.
         max_features : None or int or float
             The number of features to consider when looking for the best split.
             See the documentation for sklearn.tree.DecisionTreeRegressor for
             more details.
-        n_validation : None or int or float
+        val_size : None or int or float
             Size of the validation set to use for early stopping. If None, no
             early stopping is performed. If an int, the validation set will have
-            n_validation rows. If a float, the validation set will have
-            int(n_validation * n_samples) rows. Note that if early stopping is
-            used and max_samples is a float, the size of each subsample is
-            calculated after the validation set has been set aside.
+            val_size rows. If a float, the validation set will have
+            int(val_size * n_samples) rows. Note that if early stopping is used
+            and max_samples is a float, the size of each subsample is calculated
+            after the validation set has been set aside.
         threshold : float
             The error improvement that must be achieved every patience trees to
-            avoid early stopping. Does nothing if n_validation is None.
+            avoid early stopping. Does nothing if val_size is None.
         patience : int
             The number of trees after which early stopping will be triggered if
-            the error has not decreased by threshold. Does nothing if
-            n_validation is None.
+            the error has not decreased by threshold. Does nothing if val_size
+            is None.
         random_state : None or int or np.random.Generator
             The random state to use for subsampling and to pass to the trees in
             the forest. If None, a Generator is created with an unpredictable
@@ -73,12 +68,12 @@ class BoostedForestRegressor(BaseEstimator, RegressorMixin):
 
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
+        self.max_samples = max_samples
 
         self.max_depth = max_depth
-        self.max_samples = max_samples
         self.max_features = max_features
 
-        self.n_validation = n_validation
+        self.val_size = val_size
         self.threshold = threshold
         self.patience = patience
 
@@ -89,8 +84,8 @@ class BoostedForestRegressor(BaseEstimator, RegressorMixin):
         """
         Fit a boosted forest on the training set (X, y).
 
-        If self.n_validation is not None, then some of the provided samples will
-        be set aside to use as a validation set for early stopping.
+        If self.val_size is not None, then some of the provided samples will be
+        set aside to use as a validation set for early stopping.
 
         Parameters
         ----------
@@ -114,62 +109,52 @@ class BoostedForestRegressor(BaseEstimator, RegressorMixin):
         if random_state is None or isinstance(random_state, int):
             random_state = np.random.RandomState(random_state)
 
-        early_stopping = self.n_validation is not None
-        if early_stopping:
-            n_validation = self.n_validation
-            if isinstance(n_validation, float):
-                n_validation = int(n_validation * X.shape[0])
-
-            perm = random_state.permutation(X.shape[0])
-            idx_tr, idx_va = perm[:-n_validation], perm[-n_validation:]
-            X, y, X_va, y_va = X[idx_tr], y[idx_tr], X[idx_va], y[idx_va]
+        validate = self.val_size is not None
+        if validate:
+            X, X_va, y, y_va = train_test_split(
+                X, y,
+                test_size=self.val_size,
+                random_state=random_state
+            )
 
         n_samples = X.shape[0]
-        if self.max_samples is None:
-            max_samples = n_samples
-        elif isinstance(self.max_samples, int):
-            max_samples = self.max_samples
-        elif isinstance(self.max_samples, float):
-            max_samples = max(int(self.max_samples * n_samples), 1)
+        n_bootstrap = get_n_bootstrap(n_samples, self.max_samples)
 
         current = np.zeros(y.shape)
-        if early_stopping:
+        if validate:
             current_va = np.zeros(y_va.shape)
-            lowest_error = np.sqrt((y_va ** 2).mean())
-            trees_since_improvement = 0
+            lowest_error = get_error(y_va, current_va).mean()
+            steps_since_improvement = 0
 
-        trees = [DecisionTreeRegressor(
-            max_depth=self.max_depth,
-            max_features=self.max_features,
-            random_state=random_state
-        ) for _ in range(self.n_estimators)]
+        estimators, stop = [], False
+        for i in range(1, self.n_estimators + 1):
+            weights = get_error(y, current)
+            weights = weights / weights.sum()
+            idx = random_state.choice(n_samples, size=n_bootstrap, p=weights)
 
-        for j, tree in enumerate(trees, start=1):
-            errors = (current - y) ** 2
-            if errors.ndim > 1:
-                errors = errors.mean(axis=1)
+            estimator = DecisionTreeRegressor(
+                max_depth=self.max_depth,
+                max_features=self.max_features,
+                random_state=random_state
+            ).fit(X[idx], y[idx] - current[idx])
 
-            weights = errors / errors.sum()
-            idx = random_state.choice(n_samples, size=max_samples, p=weights)
-            tree.fit(X[idx], y[idx] - current[idx])
+            estimators.append(estimator)
+            current += self.learning_rate * estimator.predict(X)
+            message = f'Fit estimator {i}'
 
-            message = f'Fit tree {j} out of {self.n_estimators}'
-            current += self.learning_rate * tree.predict(X)
+            if validate:
+                current_va += self.learning_rate * estimator.predict(X_va)
+                error = get_error(y_va, current_va).mean()
+                message += f' - validation error is {error:.3f}'
 
-            stop = False
-            if early_stopping:
-                current_va += self.learning_rate * tree.predict(X_va)
-                error_va = np.sqrt(((current_va - y_va) ** 2).mean())
-                message += f' -- validation error is {error_va:.3f}'
-
-                if error_va < lowest_error - self.threshold:
-                    lowest_error = error_va
-                    trees_since_improvement = 0
+                if error < lowest_error - self.threshold:
+                    lowest_error = error
+                    steps_since_improvement = 0
                 else:
-                    trees_since_improvement += 1
+                    steps_since_improvement += 1
 
-                if trees_since_improvement == self.patience:
-                    message += '\nStopping early due to lack of improvement.'
+                if steps_since_improvement == self.patience:
+                    message += '\nStopping early due to lack of improvement'
                     stop = True
 
             if self.verbose:
@@ -178,7 +163,7 @@ class BoostedForestRegressor(BaseEstimator, RegressorMixin):
             if stop:
                 break
 
-        self.estimators_ = trees[:j]
+        self.estimators_ = estimators
         self.n_features_in_ = X.shape[1]
 
         return self
@@ -202,7 +187,7 @@ class BoostedForestRegressor(BaseEstimator, RegressorMixin):
         check_is_fitted(self)
         X = check_array(X)
 
-        outputs = [tree.predict(X) for tree in self.estimators_]
+        outputs = [estimator.predict(X) for estimator in self.estimators_]
 
         return self.learning_rate * sum(outputs)
 
